59497201082e34f4b8c7391a7348306a6750897f
Juan Alfieri <soldier@soldier-2.local>
2022-04-18 23:23:32 -0300

    Fix favorites

--- pages/favoritos.tsx
+++ pages/favoritos.tsx
@@ -37,7 +37,7 @@ const Favorites = observer(() => {
       <Container>
         {status === "loading" && <p>Cargando</p>}
         {status === "empty" && <p>No posee favoritos cargados</p>}
-        <PropertyList properties={favs} filters={false} />
+        <PropertyList properties={favs} filters={false} meta={{total_count: favs.length, limit: 26, offset: 0, next: '', previous: ''}} saveSearch={false}/>
         <ContactForm className='full' />
       </Container>
     </Layout>

040c67916cfa89df95f45c843c05fe1a7be3e065
Maxi Pinna <maxipinna92@gmail.com>
2022-08-13 23:59:08 -0300

    Update styles on conoce section

--- components/pages/conoceBelga.styles.ts
+++ components/pages/conoceBelga.styles.ts
@@ -1,5 +1,128 @@
 import styled from "@emotion/styled";
 
+export const OficinasSection = styled.section`
+  width: 100%;
+  background-color: ${(props: any) => props.theme.white};
+  margin: 0 auto;
+  padding: 80px 0;
+`;
+
+export const ServiciosSection = styled.section`
+  width: 100%;
+  background-color: ${(props: any) => props.theme.white};
+  margin: 0 auto;
+  padding: 80px 0;
+`;
+
+export const SomosBelgaSection = styled.section`
+  width: 100%;
+  background-color: ${(props: any) => props.theme.white};
+  margin: 0 auto;
+  padding: 80px 0;
+`;
+
+export const QuotesSection = styled.section`
+  width: 100%;
+  background-color: ${(props: any) => props.theme.white};
+  margin: 0 auto;
+  padding: 80px 0;
+`;
+
+export const ValoresListText = styled.div`
+  color: ${(props: any) => props.theme.black};
+  text-align: justify;
+  font-size: 14px;
+  line-height: 18px;
+`;
+
+export const ValoresListTitle = styled.div`
+  font-weight: 100;
+  font-size: 48px;
+  margin-bottom: 20px;
+`;
+
+export const ValoresWrapper = styled.div`
+  width: 100%;
+  max-width: 330px;
+  color: ${(props: any) => props.theme.black};
+`;
+
+export const ValoresList = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: baseline;
+  justify-content: space-between;
+`;
+
+export const ValoresSection = styled.section`
+  width: 100%;
+  background-color: ${(props: any) => props.theme.gray10};
+  margin: 0 auto;
+  padding: 80px 0;
+`;
+
+export const HistoriaText = styled.p`
+  width: 100%;
+  max-width: 600px;
+  font-size: 14px;
+  line-height: 20px;
+  margin-bottom: 20px;
+`;
+
+export const UnderlineTitle = styled.div`
+  width: 100%;
+  max-width: 600px;
+  font-size: 24px;
+  font-weight: 500;
+  margin-bottom: 50px;
+  u {
+    font-weight: 500;
+    text-decoration: underline;
+  }
+`;
+
+export const HistoriaTextWrapper = styled.div`
+  width: 100%;
+  max-width: 600px;
+  color: ${(props: any) => props.theme.black};
+`;
+
+export const HistoriaRight = styled.div`
+  width: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const HistoriaGif = styled.img`
+  width: 100%;
+  max-width: 550px;
+  height: auto;
+  margin: 0 auto;
+`;
+
+export const HistoriaLeft = styled.div`
+  width: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const HistoriaWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 3%;
+`;
+
+export const HistoriaSection = styled.section`
+  width: 100%;
+  background-color: ${(props: any) => props.theme.white};
+  margin: 0 auto;
+  padding: 100px 0;
+`;
+
 export const FormWrapper = styled.div`
   width: 100%;
   margin: 30px auto 100px;
@@ -13,8 +136,8 @@ export const BlackLayer = styled.div`
   background: rgb(0, 0, 0);
   background: linear-gradient(
     0deg,
-    rgba(0, 0, 0, 0.5) 0%,
-    rgba(0, 0, 0, 0) 20%
+    rgba(0, 0, 0, 0.75) 0%,
+    rgba(0, 0, 0, 0) 40%
   );
   position: absolute;
   top: 0;
@@ -24,27 +147,84 @@ export const BlackLayer = styled.div`
   }
 `;
 
+export const SidebarContainer = styled.div`
+  width: 100%;
+  overflow: hidden;
+  position: relative;
+`;
+
+export const SidebarRed = styled.div`
+  width: 100px;
+  height: 100%;
+  position: absolute;
+  z-index: 3;
+  clip: rect(0, auto, auto, 0);
+  top: 0;
+  right: 0;
+`;
+
 export const Hero = styled.div`
   width: 100%;
   height: 100%;
   position: absolute;
+  z-index: 4;
   clip: rect(0, auto, auto, 0);
   top: 0;
   left: 0;
 `;
 
+export const MenuHeroText = styled.div`
+  color: ${(props: any) => props.theme.white};
+  text-transform: uppercase;
+  text-align: center;
+  border-bottom: 2px solid transparent;
+  padding-bottom: 9px;
+  transition: all 0.3s ease-in-out;
+`;
+
+export const MenuHeroItem = styled.div`
+  width: 20%;
+  height: 100%;
+  align-self: start;
+  align-items: center;
+  display: flex;
+  text-align: center;
+  justify-content: center;
+  &:hover {
+    cursor: pointer;
+    ${MenuHeroText} {
+      border-color: ${(props: any) => props.theme.white};
+    }
+  }
+  &.active {
+    ${MenuHeroText} {
+      border-color: ${(props: any) => props.theme.white};
+    }
+  }
+`;
+
+export const MenuHero = styled.div`
+  width: 100%;
+  height: 100px;
+  display: flex;
+  align-items: center;
+  font-size: 14px;
+  font-weight: 500;
+  margin-top: auto;
+  z-index: 5;
+  position: relative;
+`;
+
 export const HeroWrapper = styled.div`
   width: 100%;
   height: calc(100vh - 150px);
-  //background-image: url("/../images/home_bg_hero.jpg");
+  display: flex;
+  align-items: flex-end;
   background-position: center;
   background-size: cover;
   background-repeat: no-repeat;
   overflow: hidden;
   position: relative;
-  @media screen and (max-width: 992px) {
-    //background-image: url("/../images/home_bg_hero_mobile.jpg");
-  }
   @media screen and (max-width: 840px) {
     height: auto;
     padding: 100px 0 80px;
--- pages/conoce-belga.tsx
+++ pages/conoce-belga.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Head from "next/head";
 import { observer } from "mobx-react-lite";
+import { classes } from "helpers";
 
 import { Layout, Container } from "components/layout";
 import { ContactForm } from "components/forms/contactform";
@@ -13,9 +14,33 @@ import {
   Hero,
   BlackLayer,
   FormWrapper,
+  HistoriaSection,
+  HistoriaWrapper,
+  HistoriaLeft,
+  HistoriaRight,
+  HistoriaGif,
+  HistoriaTextWrapper,
+  UnderlineTitle,
+  HistoriaText,
+  SidebarContainer,
+  SidebarRed,
+  MenuHero,
+  MenuHeroItem,
+  MenuHeroText,
+  ValoresSection,
+  ValoresList,
+  ValoresWrapper,
+  ValoresListTitle,
+  ValoresListText,
+  QuotesSection,
+  SomosBelgaSection,
+  ServiciosSection,
+  OficinasSection,
 } from "components/pages/conoceBelga.styles";
 
 const ConoceBelga = observer(() => {
+  /* Handle UI menu */
+  const [activeSection, setActiveSection] = React.useState<string>("historia");
   return (
     <Layout menuTheme="light" footerSmall backToTopFooter>
       <Head>
@@ -29,13 +54,169 @@ const ConoceBelga = observer(() => {
           <Hero>
             <SocialSidebar />
           </Hero>
+          <Container>
+            <MenuHero>
+              <MenuHeroItem
+                className={classes({ active: activeSection === "historia" })}
+                onClick={() => setActiveSection("historia")}
+              >
+                <MenuHeroText>Historia</MenuHeroText>
+              </MenuHeroItem>
+
+              <MenuHeroItem
+                className={classes({ active: activeSection === "valores" })}
+                onClick={() => setActiveSection("valores")}
+              >
+                <MenuHeroText>Valores</MenuHeroText>
+              </MenuHeroItem>
+
+              <MenuHeroItem
+                className={classes({ active: activeSection === "belga" })}
+                onClick={() => setActiveSection("belga")}
+              >
+                <MenuHeroText>Somos Belga</MenuHeroText>
+              </MenuHeroItem>
+
+              <MenuHeroItem
+                className={classes({ active: activeSection === "servicios" })}
+                onClick={() => setActiveSection("servicios")}
+              >
+                <MenuHeroText>Servicios</MenuHeroText>
+              </MenuHeroItem>
+
+              <MenuHeroItem
+                className={classes({ active: activeSection === "oficinas" })}
+                onClick={() => setActiveSection("oficinas")}
+              >
+                <MenuHeroText>Oficinas</MenuHeroText>
+              </MenuHeroItem>
+            </MenuHero>
+          </Container>
         </HeroWrapper>
 
-        <Container>
+        <SidebarContainer>
+          <SidebarRed>
+            <SocialSidebar color="red" />
+            <BackToTop color="red" />
+          </SidebarRed>
+
+          <HistoriaSection>
+            <Container>
+              <HistoriaWrapper>
+                <HistoriaLeft>
+                  <HistoriaGif src="/images/65_historia.gif" />
+                </HistoriaLeft>
+                <HistoriaRight>
+                  <HistoriaTextWrapper>
+                    <UnderlineTitle>
+                      Una <u>Historia</u> Belga
+                    </UnderlineTitle>
+                    <HistoriaText>
+                      Belga comienza su historia con nuestro fundador Jorge
+                      Jooris en 1957. Recién llegado desde Bélgica con un puñado
+                      de costumbres y tradiciones familiares al barrio de
+                      Belgrano ubicado en la Ciudad Autónoma de Buenos Aires. Un
+                      hombre apasionado por su profesión que supo conectar a
+                      cientos de personas con los que terminarían siendo sus
+                      nuevos hogares. Una pasión que transmitió a la siguiente y
+                      actual generación.
+                    </HistoriaText>
+                    <HistoriaText>
+                      Belgrano y Palermo son las sedes dónde tenemos nuestras 3
+                      casas, nuestros hogares, cómo nos gusta llamarles. Barrios
+                      que fueron y son testigos del esfuerzo, la dedicación y el
+                      compromiso que nos sigue haciendo crecer, y acompañando
+                      después de 65 años.
+                    </HistoriaText>
+                    <HistoriaText>
+                      Hoy contamos con un cálido grupo humano de profesionales
+                      destacados que brindan al mercado un asesoramiento
+                      inmobiliario distinguido.
+                    </HistoriaText>
+                    <HistoriaText>
+                      Somos el lugar donde nacen las historias.
+                    </HistoriaText>
+                  </HistoriaTextWrapper>
+                </HistoriaRight>
+              </HistoriaWrapper>
+            </Container>
+          </HistoriaSection>
+
+          <ValoresSection>
+            <Container>
+              <UnderlineTitle>
+                Los <u>Valores</u> que Jorge nos dejó
+              </UnderlineTitle>
+
+              <ValoresList>
+                <ValoresWrapper>
+                  <ValoresListTitle>Una tradición familiar.</ValoresListTitle>
+                  <ValoresListText>
+                    Somos una empresa reconocida por su trato familiar, una de
+                    nuestras prioridades es mantener un trato personalizado que
+                    nos permita entender y empatizar con cada situación.
+                    Buscando como resultado que nuestros clientes se sientan
+                    acompañados en todas las etapas de cada operación.
+                  </ValoresListText>
+                </ValoresWrapper>
+
+                <ValoresWrapper>
+                  <ValoresListTitle>Una puerta abierta.</ValoresListTitle>
+                  <ValoresListText>
+                    Creemos fielmente que la confianza y la honestidad son los
+                    pilares fundamentales de cualquier forma de trabajo. Es por
+                    esto que, a través de todos estos años, hemos cosechado
+                    fuertes lazos con todos nuestros clientes, manteniendo
+                    absoluta transparencia en todas nuestras operaciones.
+                  </ValoresListText>
+                </ValoresWrapper>
+
+                <ValoresWrapper>
+                  <ValoresListTitle>Un desarrollo constante.</ValoresListTitle>
+                  <ValoresListText>
+                    El crecimiento siempre viene acompañado del aprendizaje. En
+                    un mundo donde los cambios son constantes, es imprescindible
+                    capacitarnos constantemente para estar conectados y
+                    actualizarnos con las últimas tendencias en tecnología. Es
+                    por eso que ofrecemos servicios de excelencia.
+                  </ValoresListText>
+                </ValoresWrapper>
+              </ValoresList>
+            </Container>
+          </ValoresSection>
+
+          <QuotesSection></QuotesSection>
+
+          <SomosBelgaSection>
+            <Container>
+              <UnderlineTitle>
+                <u>Somos Belga</u>
+              </UnderlineTitle>
+            </Container>
+          </SomosBelgaSection>
+
+          <ServiciosSection>
+            <Container>
+              <UnderlineTitle>
+                Los <u>Servicios</u> que ofrecemos
+              </UnderlineTitle>
+            </Container>
+          </ServiciosSection>
+
+          <OficinasSection>
+            <Container>
+              <UnderlineTitle>
+                Nuestras<u>Oficinas</u>, nuestras casas
+              </UnderlineTitle>
+            </Container>
+          </OficinasSection>
+
           <FormWrapper>
-            <ContactForm className="full" />
+            <Container>
+              <ContactForm className="full" />
+            </Container>
           </FormWrapper>
-        </Container>
+        </SidebarContainer>
       </ConoceBelgaContainer>
     </Layout>
   );

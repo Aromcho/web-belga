bc9ed0b1351ca33367babd27fae857a0c089434f
Arom Aguilar <barriosarom@gmail.com>
2024-08-04 19:01:10 -0300

    diseño terminado falta el footer de cada pagina

--- components/print/styles.styles.ts
+++ components/print/styles.styles.ts
@@ -3,20 +3,169 @@ import { css } from "@emotion/react";
 
 export const PropertyPDF = styled.table`
   width: 1100px;
+  height: auto;
   font-family: "Montserrat", sans-serif;
-  font-weight: normal;
 `;
-
 export const Header = styled.div`
+  position: relative;
   width: 100%;
   background: white;
   display: flex;
   align-items: flex-start;
   text-align: left;
-  position: relative;
-  margin-bottom: 25px;
+  margin-bottom: 15px;
 `;
 
+
+/* new version */
+export const AddressSecundaryTitle = styled.p`
+  font-size: 24px;
+  line-height: 26px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-top: 15px;
+  span {
+    margin: 0 7px;
+  }
+    margin-left: 8px;
+    font-weight: 500;
+
+`;
+
+export const AddressTitle = styled.span`
+  font-size: 44px;
+  `;
+export const TitleNew = styled.span`
+   padding-left: 35px;
+  font-size: 24px;
+  line-height: 26px;
+  font-weight: 700;
+  span {
+    margin: 0 7px;
+  }
+  display: flex;
+  flex-direction: column;
+  align-items: start;
+  justify-content: center;
+`;
+export const Price = styled.span`
+  height: 50%;
+  font-size: 40px;
+  font-weight: 600;
+  text-align: end;
+`;
+export const ImgLogo = styled.img`
+  width: 165px;
+  height: 40px;
+  margin-right: 30px;
+  margin-top: 40px;
+`;
+export const MainPriceCoin = styled.span`
+  font-size: 25px;
+  font-weight: 600;
+  color: black;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-right: 10px;
+`;
+
+export const MainPriceContainer = styled.div`
+  
+   margin-right: 30px;
+   margin-left: 30px;
+   margin-top: 30px;
+  margin-bottom: 30px;
+`;
+export const MainPrice = styled.span`
+  font-size: 30px;
+  font-weight: 600px;
+  color: black;
+  display: flex;
+  align-items: end;
+  justify-content: end;
+  margin-bottom: 10px;
+
+  `;
+
+export const MainTitlecontainer = styled.div`
+  position: relative;
+  top:-48px;
+  display: flex;
+  justify-content: space-between;
+  background-color: white;
+  width: 100vw;
+  padding-right: 10px;
+`;
+export const CenterHeader = styled.div`
+  position: absolute;
+  top: -10px;
+  width: 100%;
+  height: 100px;
+  background: #F1B332;
+  display: flex;
+  justify-content: end;
+  align-items: flex-center;
+  text-align: left;
+`;
+
+export const CenterHeaderContent = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  padding: 14px 0 14px 22px;
+  border-bottom: 1px solid #333;
+`;
+
+export const CenterHeaderTitle = styled.span`
+  font-size: 24px;
+  line-height: 26px;
+  font-weight: 700;
+`;
+
+export const CenterHeaderSubtitle = styled.span`
+  font-size: 18px;
+  line-height: 22px;
+  font-weight: 400;
+  margin-top: 10px;
+`;
+
+export const CenterColumn = styled.div`
+  width: 100%;
+  text-align: center;
+  margin: 0;
+  padding: 0;
+`;
+
+export const GridContainer = styled.div`
+position: relative;
+  top:-52px;
+  display: flex;
+  flex-direction: row;
+  width: 100%;
+  margin: 0;
+  padding-left: 35px;
+  padding-right: 35px;
+  gap: 20px;
+`;
+
+export const MainImage = styled.div`
+  flex: 3;
+`;
+
+export const SmallImages = styled.div`
+  width: 100%;
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+    gap: 20px;
+`;
+export const Rooms = styled.p`
+  mergitn-top: 10px;
+
+`;
+/*------------------------------*/
 export const Logo = styled.img`
   width: 130px;
 `;
@@ -26,7 +175,6 @@ export const HeaderContent = styled.div`
   display: flex;
   flex-direction: column;
   padding: 14px 0 14px 22px;
-  border-bottom: 1px solid black;
 `;
 
 export const HeaderTitle = styled.span`
@@ -36,7 +184,7 @@ export const HeaderTitle = styled.span`
   span {
     margin: 0 7px;
   }
-`
+`;
 
 export const HeaderSubtitle = styled.span`
   font-size: 18px;
@@ -47,12 +195,12 @@ export const HeaderSubtitle = styled.span`
   max-width: 940px;
   overflow: hidden;
   white-space: nowrap;
-`
+`;
 
 export const HeaderLabel = styled.span`
   font-size: 10px;
   font-weight: 500;
-  color: #C62025;
+  color: #c62025;
   position: absolute;
   right: 0;
   bottom: 9px;
@@ -61,7 +209,7 @@ export const HeaderLabel = styled.span`
 
 export const LeftColumn = styled.div`
   width: 50%;
-`;
+  `;
 
 export const RightColumn = styled.div`
   width: 50%;
@@ -89,7 +237,7 @@ export const IconLabel = styled.span`
 
 export const IconImage = styled.img`
   width: 46px;
-  &.fix{
+  &.fix {
     width: 60px;
   }
 `;
@@ -98,41 +246,39 @@ export const DescriptionText = styled.div`
   font-size: 16px;
   line-height: 19px;
   font-weight: 500;
-  b {
-    font-weight: 700;
-  }
-
-  i {
-    font-style: italic;
-  }
 `;
 
 export const GalleryList = styled.div`
   display: flex;
-  gap: 22px;
-  flex-flow: wrap;
-  &.fixes{
-    margin-top: 30px;
+  gap: 15px; /* Ajustar el espacio entre las imágenes */
+  flex-wrap: wrap;
+  &.fixes {
+    margin-top: 15px;
   }
+  
 `;
 
 export const GalleryItem = styled.div`
   width: calc(50% - 11px);
   height: 158px;
-  /* aspect-ratio: 2/1; */
   background-size: cover;
   background-repeat: no-repeat;
   background-position: center;
+  margin-bottom: 15px; /* Asegurar el mismo espacio vertical */
 `;
 
 export const GalleryItemImage = styled.img`
   width: calc(50% - 8px);
-`;
+  
+  `;
+
 
 export const ContentWrapper = styled.div`
   align-items: flex-start;
   display: flex;
-  gap: 30px;
+  gap: 15px;
+  padding: 0 35px;
+
 `;
 
 export const BottomWrapper = styled.div`
@@ -149,40 +295,20 @@ export const BottomLeft = styled.div`
 export const BottomRight = styled.div`
   width: 150px;
   height: 150px;
-  border: 1px solid #C62025;
+  border: 1px solid #c62025;
   display: flex;
   align-items: center;
   justify-content: center;
 `;
 
-export const Rooms = styled.span`
-  height: 50%;
-  font-size: 40px;
-  text-align: center;
-  padding: 15px 0;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-`;
 
-export const Price = styled.span`
-  height: 50%;
-  font-size: 40px;
-  font-weight: 600;
-  color: white;
-  background-color: #C62025;
-  text-align: center;
-  padding: 15px 0;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-`;
+
 
 export const PrintStyle = css`
   #pdfItem {
     display: none;
   }
-  
+
   @media print {
     #pdfItem {
       display: block;
@@ -208,21 +334,8 @@ export const PrintStyle = css`
     }
   }
 
-  @page { 
+  @page {
     size: A4 landscape;
     margin: 70px 35px;
   }
-`;
-
-export const Divider = styled.hr`
-  width: 100%;
-  height: 4px;
-  background-color: #000000;
-  margin: 30px 0;
-`;
-
-export const Disclaimer = styled.p`
-  font-size: 10px;
-  color: #000000;
-  font-weight: 400;
 `;

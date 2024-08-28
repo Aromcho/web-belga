5dc9d59476ae034c4f9d84cc2cff96bb912ad2c0
Aroncho <barriosarom@gmail.com>
2024-08-28 18:05:48 +0000

    añadi el ecosistema

---
+++ ecosystem.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  apps: [
+    {
+      name: "web-belga",
+      script: "npm",
+      args: "start",
+      instances: "max", // PM2 ejecutará una instancia por cada núcleo de CPU disponible
+      exec_mode: "cluster", // Modo clúster
+      env: {
+        NODE_ENV: "development",
+        PORT: 3000
+      },
+      env_production: {
+        NODE_ENV: "production",
+        PORT: 8001
+      }
+    }
+  ]
+};

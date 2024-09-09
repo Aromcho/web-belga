caa72874912abb95800daef46076bcd6c3c2e49c
Arom Aguilar <barriosarom@gmail.com>
2024-09-09 17:29:11 -0300

    iconos de layout con react-bootstrap-icons agregados v-1

--- components/layout/layout.tsx
+++ components/layout/layout.tsx
@@ -6,6 +6,7 @@ import { Global as GlobalStyling } from "@emotion/react";
 import { MenuProps } from "components/menu/menu";
 import { FooterProps } from "components/footer/footer";
 import { MailIconSend, TelIcon, WhatsappIcon } from "components/icons";
+import { EnvelopeFill, TelephoneFill } from "react-bootstrap-icons";
 
 const Menu = dynamic<MenuProps>(() => import("components/menu").then((mod) => mod.Menu))
 const Footer = dynamic<FooterProps>(() => import("components/footer").then((mod) => mod.Footer))
@@ -85,11 +86,15 @@ export const Layout = ({ children, menuTheme, footerSmall }: LayoutProps) => {
       <HeroFooter id="menuFooter">
         <Link href="tel:+541152633393">
           <a className="head--footer-link phone">
-            <TelIcon />
+          <TelephoneFill /> 
+          {/* <TelIcon /> */}
+            
           </a>
         </Link>
         <a href="mailto:info@belga.com.ar" className="head--footer-link mail" target="_blank">
-          <MailIconSend />
+          {/* <MailIconSend /> */}
+          
+          <EnvelopeFill />
         </a>
         <a href="https://api.whatsapp.com/send?phone=5491152633393&text=Hola%20Belga!%20%F0%9F%91%8B%20Quisiera%20hacerles%20una%20consulta." className="head--footer-link wsp" target="_blank">
           <WhatsappIcon />
